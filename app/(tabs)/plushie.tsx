340b81bac3e15e558b5497eb28eb9f1e240fc723
FrewinTan <frewin87@gmail.com>
2025-11-06 22:31:40 +0800

    Initial Commit

---
+++ app/(tabs)/plushie.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+const Plushie = () => {
+  return (
+    <SafeAreaView>
+      <View>
+        <Text>plushie</Text>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export default Plushie;
---
+++ app/components/WardrobeButton.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const WardrobeButton = () => {
+	return (
+		<View>
+			<Text>WardrobeButton</Text>
+		</View>
+	)
+}
+
+export default WardrobeButton
